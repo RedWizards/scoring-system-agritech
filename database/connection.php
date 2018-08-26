ea964fd725d1ff2b3861ad8f0370df4dc3e40597
Red Periabras <redperiabras@gmail.com>
2018-08-26 10:34:49 +0800

    Spcific port

--- database/connection.php
+++ database/connection.php
@@ -1,11 +1,13 @@
 
 <?php
-	$host = "localhost";
+	$host = "127.0.0.1";
 	$user = "root";
 	$password = "";
 	$dbName = "scoring";
-	$conn = new mysqli($host, $user, $password, $dbName);
+	$port = 3307;
+	$conn = new mysqli($host, $user, $password, $dbName, $port);
+
 	if($conn->connect_errno){
 		echo "Failed to connect to MySQL: ( ".$conn->connect_errno ." ) ".$conn->connect_error;
 	}
-?> 
+?>
